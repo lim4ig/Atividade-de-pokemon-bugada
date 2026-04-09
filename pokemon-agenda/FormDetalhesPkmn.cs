abf5f9bcc3f437ec8f9a79884143e81d941d1b0e
lima <igorrmatheus2005@gmail.com>
2026-04-09 20:40:10 -0300

    ..............

---
+++ pokemon-agenda/FormDetalhesPkmn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pokemon_agenda
+{
+    public partial class FormDetalhesPkmn : Form
+    {
+        public Pokemon pkmnRecebido { get; set; }
+
+        public FormDetalhesPkmn()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDetalhesPkmn_Load(object sender, EventArgs e)
+        {
+            lbNome.Text = "Nome: " + pkmnRecebido.NomePkmn;
+            lbTipo.Text = "Tipo: " + pkmnRecebido.TipoPkmn;
+            lbNivel.Text = "Nível: " + pkmnRecebido.NivelPkmn.ToString();
+        }
+    }
+}
